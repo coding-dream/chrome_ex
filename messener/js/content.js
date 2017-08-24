fb1cc3c87189cfacbc255e98f9497f65eb4d5acc
deeper <deeper@gmail.com>
2017-08-24 10:59:10 +0800

    fix bugs

--- messener/js/content.js
+++ messener/js/content.js
@@ -1,14 +1,20 @@
 /**
  content_scripts可以共享页面的DOM,但是js相互隔离，互不影响。
-
-
+ 由于content_scripts和inject_scripts 都算是注入到某个url页面中（好像是页面的一部分），所以不支持跨域，而popup和background算是插件的一部分，所以都支持跨域。
 */
 $(function(){
+	var url = window.location.href;// 获取当前请求的url,可以根据url处理不同的逻辑。
 	var content = $("body").html();
-	// alert(content);
-	$("a").addClass("plugin_a");
+	// $("a").addClass("plugin_a");
 	// $("h1,h2,p").removeClass("blue");// 能操作DOM自然可以删除or添加页面<a class='blue'/>等class属性（操作的不是css，所以都是可以的。）
 
-
+	
 
 });
+
+window.onmouseup = function(){ // 用户鼠标松开时候触发事件
+    var selection = window.getSelection(); // 当前鼠标选择的文本
+    if(selection.anchorOffset != selection.extentOffset){
+        chrome.runtime.sendMessage(selection.toString());
+    }
+}
--- messener/js/popup.js
+++ messener/js/popup.js
@@ -1,10 +1,13 @@
 // 需要区分网站DOM和扩展DOM，popup.js(background.js)操作的DOM是扩展popup.html(background.html)中的DOM, 不可以直接访问【网站页面】的DOM
 // 虽然popup和background都不能获取网站页面DOM，但是它们的chrome API比较丰富，我们常用的是这些东西就够了。
 
-$("button#login").click(function(){
-	alert("登录");
+$(function(){
+	var content = $("body").html(); 
+	alert(content);
+
 });
 
-$("button#logout").click(function(){
-	alert("注销");
+
+$("button").click(function(){
+	chrome.tabs.create({url: 'http://www.w3school.com.cn/tiy/t.asp?f=html5_video'});
 });
