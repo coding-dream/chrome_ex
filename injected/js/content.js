a8d24be0007af8928499b29c20048a53b8014242
deeper <deeper@gmail.com>
2017-08-25 22:53:47 +0800

    解释某些用法

--- injected/js/content.js
+++ injected/js/content.js
@@ -38,29 +38,29 @@ function injectCustomJs(jsPath)
 	temp.setAttribute('type', 'text/javascript');
 	// 获得的地址类似：chrome-extension://ihcokhadfjfchaeagdoclpnjdiokfakg/js/inject.js
 	temp.src = chrome.extension.getURL(jsPath); // getURL() 需要 web_accessible_resources": ["js/inject.js"] , 之前的Demo中使用扩展的图片时候提到过这个设置。
-	temp.onload = function()
-	{
-		// 放在页面不好看，onload内说明此js已经载入执行完成(oooooooooooooooooooooooooooooooooooooooooooooo)，执行完后移除掉
-		this.parentNode.removeChild(this);
-	};
 	document.body.appendChild(temp);
+
+	temp.onload = function(){ // 该元素已经载入完成。
+		console.log("temp.onload" + this); // 这里的this指的是temp元素即 HTMLScriptElement
+		// 放在页面不好看，执行完后移除掉,发现一个结论：删除掉<script src>后,script仍然有效，因为脚本已经加载内存中，而且【源码】 和 【控制台源码】几乎是天壤之别，【源码】几乎看不出任何信息，连js都没有，真正的源码是【控制台源码】 
+		this.parentNode.removeChild(this); // 删掉 <script type="text/javascript" src="chrome-extension://mpkohggclgpaapjibodgpjaccnofbcnf/js/inject.js"></script>
+	};
+	console.log("body.appendChild(temp)"); // 这个不管放在哪里都比temp.onload执行要早。temp.onload要等到页面载入完成后回调。
 }
 
 function fuckBaiduAD(){
-		// body head(或body中) append : style 对象, 但是也是有id等属性的。
 		/**
-			虽然在html中是下面的样式
-			style{
-				#content_right{ display:none; }
-				.btn_info{ display:none; }
-			}
+			<style id = "my_create_css">
+				.ad_right{ display:none; }
+				.ad_recomment{ display:none; }
+			</style>
 		*/
 		var temp = document.createElement('style');
 		temp.id = 'my_create_css';
 		(document.head || document.body).appendChild(temp);
 		var css = `
 			.ad_right{ display:none;} 			/* 移除广告 */
-			.ad_recomment{display:none;}'			/* 覆盖推荐 */
+			.ad_recomment{display:none;}			/* 覆盖推荐 */
 		`;
 		temp.innerHTML = css;
 		console.log('已注入自定义CSS！');
